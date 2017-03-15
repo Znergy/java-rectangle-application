1433d99b78de06c4b8069b10e9ecb159e0907f2e
Ryan <ryanjonesirl@gmail.com>
2017-03-15 13:37:54 -0700

    add the user interface, allowing the user to type a length and width, then know whether his rectangle is a square or not

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,18 @@
+import java.io.Console;
+
 public class App {
 
   public static void main(String[] args) {
+
+    Console myConsole = System.console();
+    System.out.println("Enter a length");
+    int length = Integer.parseInt(myConsole.readLine());
+    System.out.println("Enter a width");
+    int width = Integer.parseInt(myConsole.readLine());
+
+    Rectangle rectangle = new Rectangle(length, width);
+    boolean isSquare = rectangle.isSquare();
+    System.out.println("Is your rectangle a square? " + isSquare);
     
   }
 }
