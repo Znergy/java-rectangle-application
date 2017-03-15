872040e06c026ebedacd7237e02e81e7172e162e
Ryan <ryanjonesirl@gmail.com>
2017-03-15 13:19:09 -0700

    add the code to construct new rectangles using a constructor

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,3 +1,18 @@
 public class Rectangle {
-  
+
+  private int sideOne;
+  private int sideTwo;
+
+  public Rectangle(int sideOne, int sideTwo) {
+    this.sideOne = sideOne;
+    this.sideTwo = sideTwo;
+  }
+
+  public int getSideOne() {
+    return this.sideOne;
+  }
+
+  public int getSideTwo() {
+    return this.sideTwo;
+  }
 }
